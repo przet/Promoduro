190984fe739cec56b0db0efea90314c32c9ef465
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-07-13 19:37:21 +0930

    QT and WPF WIPs

---
+++ MainApp/mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QTimer>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->lcdNumber_countdown_timer->display(10.30);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ Promoduro_cs/Promoduro_cs/Promoduro_cs/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading; // DispatcherTimer
+
+namespace Promoduro_cs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer _timer;
+        TimeSpan _time;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            EventHandler();            
+
+
+
+
+
+        }
+
+        private void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement fe = sender as FrameworkElement;
+            ((MainWindow)fe.DataContext).TimerStart(_timer, _time);
+        }
+
+        private void TimerStart(DispatcherTimer timer, TimeSpan time)
+        {
+            _time = TimeSpan.FromSeconds(10);
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                    tbTime.Text = _time.ToString("c");
+                    if (_time == TimeSpan.Zero) _timer.Stop();
+                    _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+            timer.Start();
+        }
+    }
+}
