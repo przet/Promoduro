d86b69eb8e758ad97d9b6aa25910208cdfaab77b
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-07-15 18:48:46 +0930

    Direct Stratton copy - working properly

---
+++ promoduro_winforms/CountdownTimer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+
+namespace CountdownTimer
+{
+    partial class FrmTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTimer = new System.Windows.Forms.MaskedTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Location = new System.Drawing.Point(287, 146);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.Size = new System.Drawing.Size(100, 29);
+            this.txtTimer.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(259, 202);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(178, 92);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(142, 343);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(216, 84);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "button1";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(456, 332);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(194, 106);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "button1";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // FrmTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtTimer);
+            this.Name = "FrmTimer";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txtTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ promoduro_winforms/CountdownTimer/Form1.cs
@@ -0,0 +1,78 @@
+﻿// FULL CREDIT TO https://www.geoffstratton.com/cnet-countdown-timer
+// Cut + paste from that site for testing purposes (to have a working baseline)
+
+using System;
+using System.Windows.Forms;
+using System.Media;
+ 
+namespace CountdownTimer
+{
+    public partial class FrmTimer : Form
+    {
+        private int timeLeft;
+                 
+        public FrmTimer()
+        {
+            InitializeComponent();
+        }
+ 
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (txtTimer.Text == "00:00")
+            { 
+                MessageBox.Show("Please enter the time to start!", "Enter the Time", MessageBoxButtons.OK);
+            } 
+            else
+            {
+                // Convert text to seconds as int for timer
+                string[] totalSeconds = txtTimer.Text.Split(':');
+                int minutes = Convert.ToInt32(totalSeconds[0]);
+                int seconds = Convert.ToInt32(totalSeconds[1]);
+                timeLeft = (minutes*60) + seconds;
+ 
+                // Lock Start and Clear buttons and text box
+                btnStart.Enabled = false;
+                btnClear.Enabled = false;
+                txtTimer.ReadOnly = true;
+ 
+                // Define Tick eventhandler and start timer
+                timer1.Tick += new EventHandler(timer1_Tick);
+                timer1.Start();
+            }
+        }
+ 
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timeLeft = 0;
+            btnStart.Enabled = true;
+            btnClear.Enabled = true;
+            txtTimer.ReadOnly = false;
+        }
+ 
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTimer.Text = "00:00";
+        }
+ 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                // Display time remaining as mm:ss
+                var timespan = TimeSpan.FromSeconds(timeLeft);
+                txtTimer.Text = timespan.ToString(@"mm\:ss");
+                // Alternate method
+                //int secondsLeft = timeLeft % 60;
+                //int minutesLeft = timeLeft / 60;
+            }
+            else
+            {
+                timer1.Stop();
+                SystemSounds.Exclamation.Play();
+                MessageBox.Show("Time's up!", "Time has elapsed", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
